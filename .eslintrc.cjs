2fb0759e71673edc8395a8a299aa1171aa388faa
HassanMak29 <76273304+HassanMak29@users.noreply.github.com>
2024-01-18 22:42:51 +0100

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  ignorePatterns: [
+    "dist",
+    ".eslintrc.cjs",
+    "tailwind.config.js",
+    "postcss.config.js",
+    "vite.config.ts",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-misused-promises": [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  trailingComma: "es5",
+  semi: false,
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSameLine: true,
+  // plugins: ["prettier-plugin-tailwindcss"],
+};
